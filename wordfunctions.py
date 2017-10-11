1f6bf607f9331ac9b85493fff8e30d2c54e085d0
victormenaut <31748696+victormenaut@users.noreply.github.com>
2017-10-11 18:13:48 -0500

    Add files via upload

--- wordfunctions.py
+++ wordfunctions.py
@@ -1,8 +1,18 @@
 #Function to check words against dictionary
 
+import unittest
+
+word_dict = {}
+def create_word_dict():
+    words = open("dictionary.txt", "r")
+    for line in words:
+        line = line.strip()
+        word_dict[line] = 1
+    words.close()
 
 def wordCheck(word):
-    if word in wordList:
+    word = word.lower()
+    if word in word_dict:
         return True
     else:
         return False
@@ -11,13 +21,42 @@ def wordCheck(word):
 def userResult(wordList):
     truecount = 0
     badwords = []
-    for item in wordList:
-        if wordCheck(item):
+    if len(wordList)==1:
+        if wordCheck(wordList[0]):
             truecount +=1
         else:
-            badwords.append(word)
+            badwords.append(wordList[0])
+
+
+    else:
+        for item in wordList:
+            if wordCheck(item):
+                truecount +=1
+            else:
+                badwords.append(item)
     result = ''
     badwords = str(badwords)
-    badpercentage = str((len(wordList)-truecount)/(len(wordList))
-    result+' '+'your incorrectly spelled words are: '+badwords+" your percentage of incorrectly spelled words is "+badpercentage
+    badpercentage = str((len(wordList)-truecount)/(len(wordList)))
+    result+=' '+'your incorrectly spelled words are: '+badwords+" your percentage of incorrectly spelled words is "+badpercentage
     return (result)
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(wordCheck("Happy"), True)
+    def test1(self):
+        self.assertEqual(wordCheck("Meg"), False)
+    def test2(self):
+        self.assertEqual(userResult(["thot", "ught", "ugh"]), "result" )
+    def test3(self):
+        self.assertEqual(userResult(["jkfsdl"]), 'jfjf')
+
+
+
+def main():
+    create_word_dict()
+    mytest = MyTest()
+    mytest.test()
+    mytest.test1()
+    #mytest.test2()
+    mytest.test3()
+main()
