dfc4e6fd2cbe857261b7ce0e985b93d70a7ab258
Meg Ashby <megashby@utexas.edu>
2017-10-17 15:27:32 -0500

    Add files via upload

--- project1.py
+++ project1.py
@@ -23,6 +23,9 @@ def wordCheck(word):
     else:
         return False
 
+
+
+
 def userResult(wordList):
     #print(wordList)
     truecount = 0
@@ -40,19 +43,49 @@ def userResult(wordList):
                 badwords.append(item)
     result = ''
     #print (badwords)
-    badwords = str(badwords)
+    #badwords = str(badwords)
     badpercentage = ((len(wordList)-truecount)/(len(wordList)))
     badpercentage = str(round(badpercentage, 2))
-    result='your incorrectly spelled words are: '+badwords+" your percentage of incorrectly spelled words is "+badpercentage
+    result='your incorrectly spelled words are: '+str(badwords)+" your percentage of incorrectly spelled words is "+badpercentage
+
+    #print(badwords)
+    frequent_misspelled(badwords)
+
     print(result)
     return (result)
+
 
 def input_list_words(user_input):
     input_words = user_input.lower().split(" ")
    #print(input_words)
     #return (input_words)
     userResult(input_words)
-    return input_words
+
+def frequent_misspelled(badwords):
+    badwords_dict = {}
+    for word in badwords:
+        if word not in badwords_dict:
+            badwords_dict[word] = 1
+        else:
+            badwords_dict[word]+=1
+
+    alotmisspelled = {}
+    for item in badwords_dict.keys():
+        if badwords_dict[item] >= 2:
+            alotmisspelled[item] = badwords_dict[item]
+
+    if len(alotmisspelled) !=0:
+        print ("word misspelled :"+ " number of times misspelled" )
+        for item in alotmisspelled.keys():
+            print(item, ":", alotmisspelled[item])
+
+    return(alotmisspelled)
+
+
+
+
+
+
 
 class MyTest(unittest.TestCase):
     def test(self):
@@ -67,34 +100,32 @@ class MyTest(unittest.TestCase):
         self.assertEqual(userResult(['hello', 'can']), "your incorrectly spelled words are: [] your percentage of incorrectly spelled words is 0.0")
     def test5(self):
         self.assertEqual(input_list_words("I like corn"), ['i', 'like', 'corn'])
-    #Testing input_list_words
-    def test_InputListWords_negative(self):
-        self.assertNotEqual(input_list_words("hello"),["hello", "world"], "Test FAILED: actual should not equal expected")
-        print("test_InputListWords_negative PASS")
-    def test_InputListWords_postive(self):
-        self.assertEqual(input_list_words("hello WoRld"),["hello", "world"], "Test FAILED: actual should equal  Expected")
-        print("test_InputListWords_negative PASS")
-    def test_InputListWords_postive_len(self):
-        self.assertEqual(len(input_list_words("hello WoRld")),2, "Test FAILED: actual should equal  Expected")
-        print("test_InputListWords_positve_len PASS")
-    def test_InputListWords_positive_notAWord(self):
-        self.assertEqual(input_list_words("sSG !234Sg ss"),["ssg", "!234sg","ss"], "Test FAILED: actual should equal  Expected")
-        print("test_InputListWords_positive_notAWord PASS")
+    def test6(self):
+        self.assertEqual(input_list_words('sunflower'), ['sunflower'])
+    def test7(self):
+        self.assertEqual(frequent_misspelled(["uhhh", "uhhh", "uhhh", 'djklfdsjkl', 'hfdjkjfkd', 'ttt', 'ttt']), {'uhhh': 3, 'ttt': 2})
+    def test8(self):
+        self.assertEqual(frequent_misspelled(["dogg", "catt", "socer"]), {})
+    def test9(self):
+        self.assertEqual(frequent_misspelled([]),{})
+
 def main():
     create_word_dict()
-    userInput()
-    mytest = MyTest()
-    #mytest.test()
-    #mytest.test1()
-    #mytest.test2()
-    #mytest.test3()
-    #mytest.test4()
-    #mytest.test5()
+    #userInput()
 
-    mytest.test_InputListWords_negative()
-    mytest.test_InputListWords_postive()
-    mytest.test_InputListWords_postive_len()
-    mytest.test_InputListWords_positive_notAWord()
+   
+    mytest = MyTest()
+    mytest.test()
+    mytest.test1()
+    mytest.test2()
+    mytest.test3()
+    mytest.test4()
+    mytest.test5()
+    mytest.test6()
+    mytest.test7()
+    mytest.test8()
+    mytest.test9()
 
 
 main()
+    
